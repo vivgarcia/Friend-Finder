fb712162c54e70a7581bdc2708ca386194e92ea6
vivgarcia <viv.garcia88@gmail.com>
2020-04-22 21:38:48 -0500

    created friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+var friends = [
+    {
+        name: "Leslie",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Ann",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Ben",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Andy",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "April",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Ron",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Tammy",
+        photo: " ",
+        scores: [
+
+        ]
+    },
+    {
+        name: "Chris",
+        photo: " ",
+        scores: [
+
+        ]
+    }
+]
+
+module.exports = friends; 
--- server.js
+++ server.js
@@ -1,2 +1,8 @@
+//Dependencies
 var express = require("express");
 var path = require("path");
+// Create express server
+var app = express();
+// Sets the PORT for listener
+var PORT = process.env.PORT || 8080;
+
