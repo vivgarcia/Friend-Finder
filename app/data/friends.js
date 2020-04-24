c1f76f1bba30375bd2db35378d4d1b87c912b453
vivgarcia <viv.garcia88@gmail.com>
2020-04-24 16:40:32 -0500

    added images to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = [
     {
-        name: "Leslie",
+        name: "Leslie Knope",
         photo: "https://i.imgur.com/VW5jGa3.png",
         scores: [
             "2",
@@ -16,50 +16,59 @@ var friends = [
         ]
     },
     {
-        name: "Ann",
-        photo: " ",
+        name: "Ann Perkins",
+        photo: "https://i.imgur.com/qlxq7dN.png",
         scores: [
 
         ]
     },
     {
-        name: "Ben",
-        photo: " ",
+        name: "Ben Wyatt",
+        photo: "https://i.imgur.com/URRnSCg.png",
         scores: [
 
         ]
     },
     {
-        name: "Andy",
-        photo: " ",
+        name: "Andy Dwyer",
+        photo: "https://i.imgur.com/GeBFN4J.png",
         scores: [
 
         ]
     },
     {
-        name: "April",
-        photo: " ",
+        name: "April Ludgate Dwyer",
+        photo: "https://i.imgur.com/NtpRoXf.png",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Ron Swanson",
+        photo: "https://i.imgur.com/9oeUIof.png",
         scores: [
 
         ]
     },
     {
-        name: "Ron",
-        photo: " ",
+        name: "Chris Traeger",
+        photo: "https://i.imgur.com/rZU3eTq.png",
         scores: [
 
         ]
     },
     {
-        name: "Tammy",
-        photo: " ",
-        scores: [
-
-        ]
-    },
-    {
-        name: "Chris",
-        photo: " ",
+        name: "Tom Haverford",
+        photo: "https://i.imgur.com/Psuy5By.png",
         scores: [
 
         ]
