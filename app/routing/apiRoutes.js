108ca5ab17c9d9a1e2bf80117fc30e74e7033808
vivgarcia <viv.garcia88@gmail.com>
2020-05-17 23:07:28 -0500

    trying to make this thing work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,56 +1,65 @@
 // Load data from friends.js
 
-var friendsMatch = require("../data/friends.js");
+var friends = require("../data/friends.js");
 
 // Routing
 
 module.exports = function(app){
     // displays all possible friends
     app.get("/api/friends", function(req, res){
-        res.json(friendsMatch);
+        res.json(friends);
     });
     // Handles incoming survery results from survey.html
     app.post("/api/friends", function(req, res){
-        var newFriend = req.body.scores
-        // console.log(newFriend);
-        for(var i = 0; i < newFriend.length; i++){
-            // console.log(newFriend[i]);
-            if(newFriend[i] == "1 (Strongly Disagree)"){
-                newFriend[i] == 1;
-            }else if(newFriend[i] == "5 (Strongly Agree)"){
-                newFriend[i] == 5;
-            }else {
-                newFriend[i] = parseInt(newFriend[i]);
+        var totalDifference = 0;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+        
+        var b = userScores.map(function(item){
+            return parseInt(item, 10);
+        })
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: b
+        };
+        console.log("Name: " + userName);
+        console.log("User Score: " + userScores);
+
+        var sum = b.reduce((a, b) => a+b, 0);
+        console.log("Sum of users' scores: " + sum);
+        console.log("Difference: " + bestMatch.friendDifference);
+
+        for(var i = 0; i < friends.length; i++){
+            console.log(friends[i].name);
+            totalDifference = 0;
+            console.log("Total difference: " + totalDifference);
+            console.log("Best Math Friend Difference " + bestMatch.friendDifference);
+
+            var bFriendScore = friends[i].scores.reduce((a, b) => a+b, 0);
+            console.log("Total Friend Score: " + bFriendScore);
+            totalDifference += Math.abs(sum - bFriendScore);
+            console.log("---------------> " + totalDifference);
+
+            if(totalDifference <=  bestMatch.friendDifference){
+                bestMatch.name = friend[i].name;
+                bestMatch.photo = friend[i].photo;
+                bestMatch.friendDifference = totalDifference;
             }
+            console.log(totalDifference + " Total Difference ")
         }
 
-        var comparisonArray = [];
+        console.log(bestMatch);
+        friends.push(userData);
+        console.log("New User Added")
+        console.log(userData);
+        res.json(bestMatch);
 
-        for(var i = 0; i < friendsMatch.length-1; i++){
-            var comparedFriend = friendsMatch[i];
-            // console.log(friendsMatch[i]);
-
-            var difference = 0;
-
-            for(var j = 0; j < comparedFriend.scores.length; j++){
-                // console.log(comparedFriend.scores[j]);
-                var differenceOfScore = Math.abs(comparedFriend.scores[j] - newFriend[j])
-                difference += differenceOfScore;
-            }
-            comparisonArray[i] = difference;
-            console.log(differenceOfScore);
-        }
-        var bestfriendNum = comparisonArray[0];
-        var bestFriend = 0;
-
-        for(var k = 0; k < comparisonArray.length; k++){
-            if(comparisonArray[i] < bestfriendNum){
-                bestfriendNum = comparisonArray[i];
-                bestFriend = i;
-            }
-        }
-        friendsMatch.push(newFriend);
-
-        res.json(friendsMatch[bestFriend])
     });
 };
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 //Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
 // Create express server
 var app = express();
 // Sets the PORT for listener and allows Heroku to choose PORT
 var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vdn.api+json" }));
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static("app/public"));
 // Router
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 // Listener
-app.listen(PORT, function() {
-    console.log("Server listening on PORT: " + PORT);
-});
+app.listen(PORT, () => console.log("==> 🌎  Visit on http://localhost:" + PORT));
